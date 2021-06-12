a6b3a1ee4a2b0a51c6cd5dcef4a82cb4851f643e
Davee Sok <daveesok@gmail.com>
2021-06-12 18:29:22 -0500

    mvp met. now need to clean up and pass tests

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,49 +1,13 @@
 import re
 
 
-"""
-
-Tasks
-
-- Print Welcome Message
-  - write function for this
-
-
-- read_template function
-  - input is path to template
-  - output is a string of everything in the file
-
-
-- parse_template function
-  - input will be a string > from read_template function
-  - output will be an array with two parts
-    - stripped string
-    - a tuple with the values taken out
-
-
-- Create a function to ask user the inputs from parse_template
-
-- Once input is received, use merge function
-
-- merge function  
-  - Two inputs:
-    - the template string
-    - the users Input in a tuple
-  - Output will be a combined string
-
-- After madlib response is completed >> print response back in command line
-
-- Write the text back into a file
-
-
-"""
-
-
 def print_welcome_message():
     print(
         """
-Welcome to this madlib Game. A series of questions will be asked. 
-We will be collecting a series of words in order to complete a sentence. 
+*******  Welcome to The MadLib Game!! *******  
+A series of questions will be asked. 
+
+Please answer all questions
 
       """
     )
@@ -67,28 +31,55 @@ def parse_template(string):
       - a tuple with the values taken out
     """
 
-    # try to use regex to get everything inside {}
     pattern = r"\{([\w\s]+)\}"
     words_list = re.findall(pattern, string)
-    words_list = tuple(words_list)
+    words_tuple = tuple(words_list)
 
-    # then substitute everything inside {} with empty ''
     replace = r"\{([\w\s]+)\}"
     new_string = re.sub(replace, "{}", string)
 
-    return (new_string, words_list)
+    return (new_string, words_tuple)
 
 
 def merge(string, user_input):
     """
     - Two inputs:
       - the template string
-      - the users Input in a tuple
+      - a tuple with user input
     - Output will be a combined string
     """
-    pass
+    merged_string = string.format(*user_input)
+    return merged_string
+
+
+def ask_user_questions(words_tuple):
+    """
+    - input is a tuple of keywords
+    - output is a tuple of user answers
+    """
+
+    answers = []
+
+    for words in words_tuple:
+        response = input(f"Please enter a {words} >> ")
+        answers.append(response)
+
+    answer_tuple = tuple(answers)
+    return answer_tuple
+
+
+def main():
+    print_welcome_message()
+    raw_template = read_template("assets/dark_and_stormy_night_template.txt")
+    stripped_string, key_words = parse_template(raw_template)
+    user_response = ask_user_questions(key_words)
+    user_input_merged = merge(stripped_string, user_response)
+
+    print(user_input_merged)
+    with open("assets/users_custom_file.txt", "w") as new_file:
+        new_file.write(user_input_merged)
 
 
 if __name__ == "__main__":
 
-    print(read_template("assets/template_madlib.txt"))
+    main()
